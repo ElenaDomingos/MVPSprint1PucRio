ed1601eb88faa7291cbd1f7b02c09d05c51ad74a
Elena Domingos <elena-lezhneva@mail.ru>
2023-04-23 13:18:14 -0300

    Create __init__.py

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+from schemas.setor import SetorSchema, apresenta_setor, apresenta_setores
+from schemas.patrimonio import PatrimonioSchema, PatrimonioBuscaSchema, PatrimonioViewSchema, \
+    ListagemPatrimoniosSchema, PatrimonioDelSchema, apresenta_patrimonios, \
+    apresenta_patrimonio, apresenta_patrimonios
+from schemas.error import ErrorSchema
